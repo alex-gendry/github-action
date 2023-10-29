66d0157e681f0b46f2ba678dcfe44a9d7fdd468b
Alex Gendry <agendry@opentext.com>
2023-10-29 21:15:59 +0100

    doc: README  added

--- internal/ssc-run-copy-audit/dist/index.js
+++ internal/ssc-run-copy-audit/dist/index.js
@@ -37778,13 +37778,13 @@ function resolveVariables(s) {
 async function main() {
     var jp = __nccwpck_require__(4378);
     const ssc_target_appversion_id = core.getInput('ssc_target_appversion_id');
-    const ssc_copy_appversion_id = core.getInput('ssc_copy_appversion_id');
-    utils.debugObject(`Copying AppVersion Audit values ${ssc_copy_appversion_id} -> ${ssc_target_appversion_id}`);
-    utils.debugObject(`Get CustomTag list from AppVersion ${ssc_copy_appversion_id}`);
-    const customTags = await appversion.getAppVersionCustomTags(ssc_copy_appversion_id, 'id,guid,name,valueType,valueList');
+    const ssc_source_appversion_id = core.getInput('ssc_source_appversion_id');
+    utils.debugObject(`Copying AppVersion Audit values ${ssc_source_appversion_id} -> ${ssc_target_appversion_id}`);
+    utils.debugObject(`Get CustomTag list from AppVersion ${ssc_source_appversion_id}`);
+    const customTags = await appversion.getAppVersionCustomTags(ssc_source_appversion_id, 'id,guid,name,valueType,valueList');
     utils.debugObject(`CustomTags Qty: ${customTags?.length}`);
-    utils.debugObject(`Get vulns list from Source AppVersion ${ssc_copy_appversion_id}`);
-    const vulns = await appversion.getAppVersionVulns(ssc_copy_appversion_id, '', '', 'id,issueInstanceId,revision', 'auditValues');
+    utils.debugObject(`Get vulns list from Source AppVersion ${ssc_source_appversion_id}`);
+    const vulns = await appversion.getAppVersionVulns(ssc_source_appversion_id, '', '', 'id,issueInstanceId,revision', 'auditValues');
     utils.debugObject(`Vulns Qty: ${vulns?.length}`);
     utils.debugObject(`transpose to appversion ${ssc_target_appversion_id}`);
     await vuln.transposeToAppVersion(vulns, ssc_target_appversion_id);
--- internal/ssc-run-copy-audit/src/run.ts
+++ internal/ssc-run-copy-audit/src/run.ts
@@ -12,17 +12,17 @@ function resolveVariables(s: string): string {
 async function main(): Promise<void> {
 	var jp = require('jsonpath')
 	const ssc_target_appversion_id : number | string = core.getInput('ssc_target_appversion_id')
-	const ssc_copy_appversion_id : number | string = core.getInput('ssc_copy_appversion_id')
-	utils.debugObject(`Copying AppVersion Audit values ${ssc_copy_appversion_id} -> ${ssc_target_appversion_id}`)
-	utils.debugObject(`Get CustomTag list from AppVersion ${ssc_copy_appversion_id}`)
+	const ssc_source_appversion_id : number | string = core.getInput('ssc_source_appversion_id')
+	utils.debugObject(`Copying AppVersion Audit values ${ssc_source_appversion_id} -> ${ssc_target_appversion_id}`)
+	utils.debugObject(`Get CustomTag list from AppVersion ${ssc_source_appversion_id}`)
 	const customTags = await appversion.getAppVersionCustomTags(
-		ssc_copy_appversion_id,
+		ssc_source_appversion_id,
 		'id,guid,name,valueType,valueList'
 	)
 	utils.debugObject(`CustomTags Qty: ${customTags?.length}`)
-	utils.debugObject(`Get vulns list from Source AppVersion ${ssc_copy_appversion_id}`)
+	utils.debugObject(`Get vulns list from Source AppVersion ${ssc_source_appversion_id}`)
 	const vulns = await appversion.getAppVersionVulns(
-		ssc_copy_appversion_id,
+		ssc_source_appversion_id,
 		'',
 		'',
 		'id,issueInstanceId,revision',
